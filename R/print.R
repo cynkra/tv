6ca8605ccd5821e1babc24733cc380eff04c031f
Christoph <christoph.sax@gmail.com>
2018-09-01 14:50:59 +0200

    play-around

--- R/print.R
+++ R/print.R
@@ -1,4 +1,32 @@
 print_tv <- function(x, ...) {
-  print(rhandsontable::rhandsontable(x))
+  
+  tv.max.cells <- 1000
+  tv.max.rows <- 1000
+  tv.max.cols <- 500
+
+  # the idea is that seeing cols is more important than seeing rows
+  tv.col.precedence <- TRUE
+
+  stopifnot(tv.max.cells <= tv.max.rows * tv.max.cols)
+
+  if (nrow(x) * ncol(x) > tv.max.cells) {
+    message("tv: tibble has more than ", tv.max.cells, " cells. Output truncated.")
+    rows <- min(nrow(x), tv.max.rows)
+    cols <- min(ncol(x), tv.max.cols)
+
+    if (tv.col.precedence) {
+      rows <- floor(tv.max.cells / cols)
+    } else {
+      cols <- floor(tv.max.cells / rows)
+    }
+
+    x <- x[seq(rows), seq(cols)]
+  }
+
+  print(rhandsontable::rhandsontable(
+    x, 
+    readOnly = TRUE,
+    contextMenu = FALSE
+  )) 
   invisible(x)
 }
