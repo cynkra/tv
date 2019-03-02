c612f2be5a523aba9b6fa053f369769e0fac02f8
Christoph <christoph.sax@gmail.com>
2019-03-02 01:27:12 +0100

    use with base data.frame too

--- R/enable.R
+++ R/enable.R
@@ -4,8 +4,7 @@ NULL
 tv_enable <- function(verbose = FALSE) {
   register_s3_method("base", "print", "tbl_df", print_tv)
 
-  # Didn't work on first try
-  # register_s3_method("base", "print", "data.frame", print_tv)
+  register_s3_method("base", "print", "data.frame", print_tv)
 
   if (verbose) {
     message("TV is on!")
