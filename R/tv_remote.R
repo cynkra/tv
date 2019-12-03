cf26eaaa143e1922cf18cfb82d3d0d79402d3ebb
Christoph Sax <christoph.sax@gmail.com>
2019-12-03 11:35:31 +0100

    feedback

--- R/tv_remote.R
+++ R/tv_remote.R
@@ -14,6 +14,16 @@ tv_remote_own_session <- function() {
 
   Sys.sleep(0.5)
 
+  # prot nummer : 4096 - 2^16-
+  #
+
+  # same as getOption("shiny.port")
+
+
+  # FIXME use curl to check if there
+
+  # FIXME check
+
   if (Sys.getenv("RSTUDIO") == "1") {
     rstudioapi::viewer("http://127.0.0.1:1234")
   } else {
