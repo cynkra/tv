10b67d4cf8db4a0412b38e3f06130032e96e6282
pat-s <patrick.schratz@gmail.com>
2020-06-21 23:21:54 +0200

    install cynkratemplate

--- tic.R
+++ tic.R
@@ -5,5 +5,8 @@ do_package_checks(error_on = "error")
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
+
+  get_stage("deploy") %>%
+    add_step(step_install_github("cynkra/cynkratemplate"))
   do_pkgdown()
 }
